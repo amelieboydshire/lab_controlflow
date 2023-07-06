a87b548d895e9f62b481e00363926249e226caab
amelieboydshire <aboydshire10@gmail.com>
2023-07-06 21:46:03 +0100

    completed extension

--- src/GuessingGame.java
+++ src/GuessingGame.java
@@ -1,6 +1,30 @@
-public class GuessingGame {
+// public class GuessingGame {
+//   public static void main(String[] args) {
+//       int secretNumber = 8;
+//        int guess = 6;
 
-    public static void main(String[]){
+//        if (guess < secretNumber) {
+//            System.out.println("Too low");
+//        } else if (guess == secretNumber) {
+//            System.out.println("Correct guess");
+// } else if (guess > secretNumber) {
+//             System.out.println("Too high");
+//        }
+//    }
+//}
+
+class WeatherCheck {
+    public static void main(String[] args) {
+        int temperature = 20;
+        boolean currentlyRaining = false;
+
+        if (temperature < 15 && currentlyRaining) {
+            System.out.println("wear a waterproof coat");
+        } else {
+            System.out.println("wear shorts and sunscreen");
+        }
+
+
 
     }
 }
